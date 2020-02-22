e49e4a81fe578a393aae9ce3bd651ff84d063f55
sujinlee0616 <sjnu21@naver.com>
2020-02-23 01:36:47 +0900

    Make main window and add window

---
+++ main.js
@@ -0,0 +1,82 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow,Menu} = electron;
+
+let mainWindow;
+let addWindow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+    // Create new window
+    mainWindow = new BrowserWindow({});
+    // Load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    // Quit app when closed - 메인 window가 닫히면 앱 종료시키자.
+    mainWindow.on('closed', function(){
+        app.quit();
+    })
+
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+//  Handle create add window
+function createAddWindow(){
+    // Create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item'
+    });
+    // Load html into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    // Garbage collection handle
+    addWindow.on('close', function(){
+        addWindow=null;
+    });
+
+}
+
+// Create menu template
+const mainMenuTemplate = [
+    {
+        label:'File',
+        submenu: [
+            {
+                label: 'Add Item',
+                click(){
+                    createAddWindow(); // 함수 만들어서 호출 
+                }
+            },
+            {
+                label: 'Clear Items'
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+// If mac, add emplty object to menu
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({label: ' '});
+}
